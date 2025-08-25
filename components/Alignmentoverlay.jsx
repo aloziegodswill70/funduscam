8e74eca60b587599b6a748c0b1ed18a6745bb664
chinajaxon <njokuchina@gmail.com>
2025-08-25 02:09:33 +0100

    updated components/AlignmentOverlay.jsx

--- components/Alignmentoverlay.jsx
+++ components/Alignmentoverlay.jsx
@@ -1,10 +1,7 @@
 export default function AlignmentOverlay() {
   return (
-    <div className="pointer-events-none absolute inset-0">
-      <div className="absolute inset-[12%_8%] rounded-full border border-white/25" />
-      <div className="absolute inset-[22%_18%] rounded-full border border-white/35" />
-      <div className="absolute inset-[32%_28%] rounded-full border border-white/50" />
-      <div className="absolute left-1/2 top-1/2 -ml-[3px] -mt-[3px] w-[6px] h-[6px] rounded-full bg-white" />
+    <div className="absolute inset-0 pointer-events-none flex items-center justify-center">
+      <div className="border-4 border-red-500 rounded-full w-48 h-48"></div>
     </div>
   );
 }
